005e37f330c53b42c318eabb88edf15a794cc61b
Dmitry Lyapin <lyapindm@yandex.ru>
2023-01-17 16:28:29 +0300

    Сделал запросы к БД

--- src/books/books.service.ts
+++ src/books/books.service.ts
@@ -1,11 +1,8 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel, InjectConnection } from '@nestjs/mongoose';
-import { NotFoundException } from '@nestjs/common/exceptions';
 import { Connection, Model } from 'mongoose';
-import { ERRORS } from './books.constants';
 import { Books, BooksDocument } from './books.schema';
 import { IBookDTO } from './dto/i-book.dto';
-import { libraryDB } from './mock-libraryDB';
 
 @Injectable()
 export class BooksService {
@@ -19,35 +16,21 @@ export class BooksService {
     }
 
     getBook(id: string) {
-        const book = libraryDB.books.find((book) => book.id === id);
-        if (book) return book;
-        throw new NotFoundException(ERRORS.BOOK_NOT_FOUND);
+        const book = this.BooksModel.findById(id);
+        return book;
     }
 
     create(dto: Omit<IBookDTO, 'id'>) {
-        const id = new Date().toISOString();
-        libraryDB.books.push({ id, ...dto });
-        return this.getBook(id);
+        const book = new this.BooksModel(dto);
+        return book.save();
     }
 
     update(id: string, dto: Partial<IBookDTO>) {
-        const book = libraryDB.books.findIndex((book) => book.id === id);
-
-        if (book > -1) {
-            libraryDB.books[book] = { ...libraryDB.books[book], ...dto };
-            return libraryDB.books[book];
-        }
-
-        throw new NotFoundException(ERRORS.BOOK_NOT_FOUND);
+        const book = this.BooksModel.findOneAndUpdate({ _id: id }, dto);
+        return book;
     }
 
     delete(id: string) {
-        const book = libraryDB.books.find((book) => book.id === id);
-
-        if (book) {
-            libraryDB.books = libraryDB.books.filter((item) => item.id !== id);
-            return book;
-        }
-        throw new NotFoundException(ERRORS.BOOK_NOT_FOUND);
+        return this.BooksModel.findOneAndRemove({ _id: id });
     }
 }
