191d22b321665b7b6efc0a9a0e8fdd87b4f13d17
Dmitry Lyapin <lyapindm@yandex.ru>
2023-01-27 15:41:46 +0300

    Сделал interceptor для response

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ResponseInterceptor } from './interseptors/response/response.interceptor';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     app.setGlobalPrefix('api');
     app.enableCors();
+    app.useGlobalInterceptors(new ResponseInterceptor());
     await app.listen(3000);
 }
 bootstrap();
